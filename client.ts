554914a07020555fc973d245626418d7ed8cf13c
Laura Lynn <laura@iys.io>
2018-03-31 21:50:21 +0200

    why was all of this in spaces

--- client.ts
+++ client.ts
@@ -3,103 +3,103 @@ import * as events from 'events';
 import {Wrapper} from './wrapper';
 
 export class MikuiaClient extends events.EventEmitter {
-    private heartbeatTimer: NodeJS.Timer;
-    private name: string;
-    private token: string;
-    private wr: Wrapper;
+	private heartbeatTimer: NodeJS.Timer;
+	private name: string;
+	private token: string;
+	private wr: Wrapper;
 
-    constructor(address?: string, ports?: number[]) {
+	constructor(address?: string, ports?: number[]) {
 		super();
 		
 		if(!address) address = 'tcp://127.0.0.1';
 		if(!ports) ports = [3000, 3001];
 
-        this.name = 'plugin_' + Math.random().toString(36).slice(-10);
+		this.name = 'plugin_' + Math.random().toString(36).slice(-10);
 		this.token = '';
-        this.wr = new Wrapper(address, ports, this);
+		this.wr = new Wrapper(address, ports, this);
 
-        this.on('connected', () => {
-            this.heartbeatTimer = setInterval(() => {
-                this.heartbeat();
-            }, 10 * 1000);
-        })
+		this.on('connected', () => {
+			this.heartbeatTimer = setInterval(() => {
+				this.heartbeat();
+			}, 10 * 1000);
+		})
 
-        this.on('disconnected', () => {
-            clearInterval(this.heartbeatTimer);
-        });
-    }
+		this.on('disconnected', () => {
+			clearInterval(this.heartbeatTimer);
+		});
+	}
 
-    _sendRequest(method: string, args: Object | null) {
-        return this.wr.send({
-            method: method,
-            args: args,
-            token: this.token
-        });
-    }
+	_sendRequest(method: string, args: Object | null) {
+		return this.wr.send({
+			method: method,
+			args: args,
+			token: this.token
+		});
+	}
 
-    connect() {
-        this.wr.connect();
-    }
+	connect() {
+		this.wr.connect();
+	}
 
-    disconnect() {
-        this.wr.disconnect();
-    }
+	disconnect() {
+		this.wr.disconnect();
+	}
 
-    reconnect(delay: number) {
-        this.wr.reconnect(delay);
-    }
+	reconnect(delay: number) {
+		this.wr.reconnect(delay);
+	}
 
-    heartbeat() {
-        this._sendRequest('heartbeat', null).then(() => {
-            console.log('heartbeat!');
-        }).catch((err) => {
-            console.log('heartbeat failed.');
-            this.reconnect(1000);
-        });
-    }
+	heartbeat() {
+		this._sendRequest('heartbeat', null).then(() => {
+			console.log('heartbeat!');
+		}).catch((err) => {
+			console.log('heartbeat failed.');
+			this.reconnect(1000);
+		});
+	}
 
-    identify(name: string) {
-        // this.name = name;
-        this._sendRequest('identify', { name: name }).then((token: string) => {
-            console.log('identified');
-            this.name = name;
-            this.token = token;
+	identify(name: string) {
+		// this.name = name;
+		this._sendRequest('identify', { name: name }).then((token: string) => {
+			console.log('identified');
+			this.name = name;
+			this.token = token;
 
-            this.emit('identified');
-        }).catch((err) => {
-            console.log('failed');
-            this.reconnect(1000);
-        })
-    }
+			this.emit('identified');
+		}).catch((err) => {
+			console.log('failed');
+			this.reconnect(1000);
+		})
+	}
 
-    subscribe(topic: string) {
-        this.wr.subscribe(topic);
-    }
+	subscribe(topic: string) {
+		this.wr.subscribe(topic);
+	}
 
-    registerHandler(name: string, info: object) {
-        this._sendRequest('registerHandler', {
-            name: name,
-            info: info
-        }).then(() => {
-            console.log('registered handler: ' + name);
-            this.subscribe('event:handler:' + name);
-        }).catch((err) => {
-            console.log('failed to register handler: ' + name);
-        })
-    }
+	registerHandler(name: string, info: object) {
+		this._sendRequest('registerHandler', {
+			name: name,
+			info: info
+		}).then(() => {
+			console.log('registered handler: ' + name);
+			this.subscribe('event:handler:' + name);
+		}).catch((err) => {
+			console.log('failed to register handler: ' + name);
+		})
+	}
 
-    respond(event: object, data: object) {
-        this._sendRequest('respond', {
-            event: event,
-            data: data
-        }).then(() => {
-            console.log('successful thing something omg');
-        }).catch((err) => {
-            console.log('you fucked up');
-        })
-    }
+	respond(event: object, data: object) {
+		this._sendRequest('respond', {
+			event: event,
+			data: data
+		}).then(() => {
+			console.log('successful thing something omg');
+		}).catch((err) => {
+			console.log('you fucked up');
+		})
+	}
 
-    getExample() {
-        return this._sendRequest('getExample', null);
-    }
+	getExample() {
+		return this._sendRequest('getExample', null);
+	}
 }
--- wrapper.ts
+++ wrapper.ts
@@ -5,151 +5,151 @@ import {MikuiaClient} from './client';
 import {Tools} from './tools';
 
 export class Wrapper {
-    private promises = {};
-    private req: zmq.Socket;
-    private sub: zmq.Socket;
+	private promises = {};
+	private req: zmq.Socket;
+	private sub: zmq.Socket;
 
-    constructor(private address: string, private ports: number[], private client: MikuiaClient) {
-        this._req();
-        this._sub();
-    }
+	constructor(private address: string, private ports: number[], private client: MikuiaClient) {
+		this._req();
+		this._sub();
+	}
 
-    _req() {
-        this.req = zmq.socket('req');
+	_req() {
+		this.req = zmq.socket('req');
 
-        // this.req.on('connect', () => { console.log(cli.redBright('REQ: ') + 'connect'); });
-        this.req.on('connect_delay', () => { console.log(cli.redBright('REQ: ') + 'connect_delay'); });
-        this.req.on('connect_retry', () => { console.log(cli.redBright('REQ: ') + 'connect_retry'); });
-        this.req.on('listen', () => { console.log(cli.redBright('REQ: ') + 'listen'); });
-        this.req.on('bind_error', () => { console.log(cli.redBright('REQ: ') + 'bind_error'); });
-        this.req.on('accept', () => { console.log(cli.redBright('REQ: ') + 'accept'); });
-        this.req.on('accept_error', () => { console.log(cli.redBright('REQ: ') + 'accept_error'); });
-        this.req.on('close', () => { console.log(cli.redBright('REQ: ') + 'close'); });
-        this.req.on('close_error', () => { console.log(cli.redBright('REQ: ') + 'close_error'); });
-        // this.req.on('disconnect', () => { console.log(cli.redBright('REQ: ') + 'disconnect'); });
+		// this.req.on('connect', () => { console.log(cli.redBright('REQ: ') + 'connect'); });
+		this.req.on('connect_delay', () => { console.log(cli.redBright('REQ: ') + 'connect_delay'); });
+		this.req.on('connect_retry', () => { console.log(cli.redBright('REQ: ') + 'connect_retry'); });
+		this.req.on('listen', () => { console.log(cli.redBright('REQ: ') + 'listen'); });
+		this.req.on('bind_error', () => { console.log(cli.redBright('REQ: ') + 'bind_error'); });
+		this.req.on('accept', () => { console.log(cli.redBright('REQ: ') + 'accept'); });
+		this.req.on('accept_error', () => { console.log(cli.redBright('REQ: ') + 'accept_error'); });
+		this.req.on('close', () => { console.log(cli.redBright('REQ: ') + 'close'); });
+		this.req.on('close_error', () => { console.log(cli.redBright('REQ: ') + 'close_error'); });
+		// this.req.on('disconnect', () => { console.log(cli.redBright('REQ: ') + 'disconnect'); });
 
-        this.req.on('connect', () => {
-            console.log(cli.redBright('REQ: ') + 'connect');
+		this.req.on('connect', () => {
+			console.log(cli.redBright('REQ: ') + 'connect');
 
-            this.sub.connect(this.address + ':' + this.ports[0]);
-        });
+			this.sub.connect(this.address + ':' + this.ports[0]);
+		});
 
-        this.req.on('disconnect', () => {
-            console.log(cli.redBright('REQ: ') + 'disconnect');
+		this.req.on('disconnect', () => {
+			console.log(cli.redBright('REQ: ') + 'disconnect');
 
-            this.sub.disconnect(this.address + ':' + this.ports[0]);
+			this.sub.disconnect(this.address + ':' + this.ports[0]);
 
-            this.client.emit('disconnected');
-        });
+			this.client.emit('disconnected');
+		});
 
-        this.req.on('message', (data) => {
-            console.log(data);
-            var jsonData: Object | null = null;
+		this.req.on('message', (data) => {
+			console.log(data);
+			var jsonData: Object | null = null;
 
-            try {
-                jsonData = JSON.parse(data.toString());
-            } catch(e) {
-                console.log('Error.');
-                console.log(e);
-                return;
-            }
+			try {
+				jsonData = JSON.parse(data.toString());
+			} catch(e) {
+				console.log('Error.');
+				console.log(e);
+				return;
+			}
 
-            if(jsonData != null && jsonData['id'] != null) {
-                var defaultObject = {
-                    type: 'error',
-                    error: true,
-                    message: null
-                }
+			if(jsonData != null && jsonData['id'] != null) {
+				var defaultObject = {
+					type: null,
+					error: true,
+					message: null
+				}
 
-                jsonData = Tools.extend(defaultObject, jsonData);
+				jsonData = Tools.extend(defaultObject, jsonData);
 
-                console.log(jsonData);
+				console.log(jsonData);
 
-                if(jsonData['error']) {
-                    this.promises[jsonData['id']].reject(jsonData['message']);
-                } else {
-                    this.promises[jsonData['id']].resolve(jsonData['message']);
-                }
-            }
-        });
+				if(jsonData['error']) {
+					this.promises[jsonData['id']].reject(jsonData['message']);
+				} else {
+					this.promises[jsonData['id']].resolve(jsonData['message']);
+				}
+			}
+		});
 
-        this.req.monitor();
-    }
+		this.req.monitor();
+	}
 
-    _sub() {
-        this.sub = zmq.socket('sub');
+	_sub() {
+		this.sub = zmq.socket('sub');
 
-        // this.sub.on('connect', () => { console.log(cli.cyanBright('SUB: ') + 'connect'); });
-        this.sub.on('connect_delay', () => { console.log(cli.cyanBright('SUB: ') + 'connect_delay'); });
-        this.sub.on('connect_retry', () => { console.log(cli.cyanBright('SUB: ') + 'connect_retry'); });
-        this.sub.on('listen', () => { console.log(cli.cyanBright('SUB: ') + 'listen'); });
-        this.sub.on('bind_error', () => { console.log(cli.cyanBright('SUB: ') + 'bind_error'); });
-        this.sub.on('accept', () => { console.log(cli.cyanBright('SUB: ') + 'accept'); });
-        this.sub.on('accept_error', () => { console.log(cli.cyanBright('SUB: ') + 'accept_error'); });
-        this.sub.on('close', () => { console.log(cli.cyanBright('SUB: ') + 'close'); });
-        this.sub.on('close_error', () => { console.log(cli.cyanBright('SUB: ') + 'close_error'); });
-        this.sub.on('disconnect', () => { console.log(cli.cyanBright('SUB: ') + 'disconnect'); });
+		// this.sub.on('connect', () => { console.log(cli.cyanBright('SUB: ') + 'connect'); });
+		this.sub.on('connect_delay', () => { console.log(cli.cyanBright('SUB: ') + 'connect_delay'); });
+		this.sub.on('connect_retry', () => { console.log(cli.cyanBright('SUB: ') + 'connect_retry'); });
+		this.sub.on('listen', () => { console.log(cli.cyanBright('SUB: ') + 'listen'); });
+		this.sub.on('bind_error', () => { console.log(cli.cyanBright('SUB: ') + 'bind_error'); });
+		this.sub.on('accept', () => { console.log(cli.cyanBright('SUB: ') + 'accept'); });
+		this.sub.on('accept_error', () => { console.log(cli.cyanBright('SUB: ') + 'accept_error'); });
+		this.sub.on('close', () => { console.log(cli.cyanBright('SUB: ') + 'close'); });
+		this.sub.on('close_error', () => { console.log(cli.cyanBright('SUB: ') + 'close_error'); });
+		this.sub.on('disconnect', () => { console.log(cli.cyanBright('SUB: ') + 'disconnect'); });
 
-        this.sub.on('connect', () => {
-            this.client.emit('connected');
-        });
+		this.sub.on('connect', () => {
+			this.client.emit('connected');
+		});
 
-        this.sub.on('message', (topic, message) => {
-            var data = JSON.parse(message.toString());
+		this.sub.on('message', (topic, message) => {
+			var data = JSON.parse(message.toString());
 
-            this.client.emit(topic.toString(), data);
+			this.client.emit(topic.toString(), data);
 
-            // console.log('(' + cli.greenBright(data.channel) + ') ' + cli.yellowBright(data.user['display-name']) + ': ' + data.message);
+			// console.log('(' + cli.greenBright(data.channel) + ') ' + cli.yellowBright(data.user['display-name']) + ': ' + data.message);
 
-            // console.log(JSON.parse(message.toString()));
-        });
+			// console.log(JSON.parse(message.toString()));
+		});
 
-        this.sub.monitor();
-    }
+		this.sub.monitor();
+	}
 
-    connect() {
-        this.req.connect(this.address + ':' + this.ports[1]);
-        // this.sub.connect(this.address + ':' + this.ports[0]);
-    }
+	connect() {
+		this.req.connect(this.address + ':' + this.ports[1]);
+		// this.sub.connect(this.address + ':' + this.ports[0]);
+	}
 
-    disconnect() {
-        this.req.disconnect(this.address + ':' + this.ports[1]);
-        this.sub.disconnect(this.address + ':' + this.ports[0]);
-        this.client.emit('disconnected');
-    }
+	disconnect() {
+		this.req.disconnect(this.address + ':' + this.ports[1]);
+		this.sub.disconnect(this.address + ':' + this.ports[0]);
+		this.client.emit('disconnected');
+	}
 
-    reconnect(delay: number) {
-        this.disconnect();
-        setTimeout(() => {
-            this.connect()
-        }, delay);
-    }
+	reconnect(delay: number) {
+		this.disconnect();
+		setTimeout(() => {
+			this.connect()
+		}, delay);
+	}
 
-    send(message: Object | string) {
-        var object = message;
-        if(typeof object == 'string') {
-            object = {
-                message: object
-            }
-        }
+	send(message: Object | string) {
+		var object = message;
+		if(typeof object == 'string') {
+			object = {
+				message: object
+			}
+		}
 
-        var amazingId = Math.random().toString(36).slice(-10);
-        object['id'] = amazingId;
-        
-        var promise = new Promise((resolve, reject) => {
-            this.promises[amazingId] = {
-                resolve: resolve,
-                reject: reject
-            }
-        });
+		var amazingId = Math.random().toString(36).slice(-10);
+		object['id'] = amazingId;
+		
+		var promise = new Promise((resolve, reject) => {
+			this.promises[amazingId] = {
+				resolve: resolve,
+				reject: reject
+			}
+		});
 
-        // this.callbacks[amazingId];
-        this.req.send(JSON.stringify(object));
+		// this.callbacks[amazingId];
+		this.req.send(JSON.stringify(object));
 
-        return promise;
-    }
+		return promise;
+	}
 
-    subscribe(topic: string) {
-        this.sub.subscribe(topic);
-    }
+	subscribe(topic: string) {
+		this.sub.subscribe(topic);
+	}
 }
